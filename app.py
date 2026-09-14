236c4abf8d8908ff9850eb12d44429f308232c23
Josh <joshuad467@nycstudents.net>
2026-09-14 10:21:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+def rectangle():
+    t.forward(125)
+    t.left(100)
+    t.forward(125)
+    t.left(100)
+    t.forward()
+rectangle()
+
+
+turtle.done()
